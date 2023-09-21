d1882b3647d6a8815c452b4a00d014721d753298
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-09-21 18:34:55 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,45 @@ namespace Methods
 {
     class Program
     {
-        static void main (string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World");
+            /*
+            erişim_belirteci  geri_dönüş_tipi  metot_adi(parametreListesi/arguman){
+             //komutlar
+            }
+            */
+            int a =2;
+            int b=3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            
+            int sonuc2 = ornek.ArttirveTopla(ref a,ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+        }
+        static int Topla(int deger1,int deger2){
+            return(deger1+deger2);
+        }
+
+
+    }
+
+    class Metotlar{
+        public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirveTopla(ref int deger1,ref int deger2){
+            deger1+=1;
+            deger2+=1;
+            return deger1+deger2;
         }
     }
-}
+}   
