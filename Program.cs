00ba16d91066f88fc4d9ab0b0db06891bac1f4f2
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-09-24 16:40:07 +0300

    Overloading

--- Program.cs
+++ Program.cs
@@ -26,12 +26,32 @@ namespace Methods
             ornek.EkranaYazdir(Convert.ToString(sonuc2));
             ornek.EkranaYazdir(Convert.ToString(a+b));
 
+            /////////////////////////
+            //out parametreler
+            /////////////////////////
+            string sayi = "999";
+
+            bool sonucOut = int.TryParse(sayi, out int outSayi);
+            if(sonucOut){
+                Console.WriteLine("Başarılı " + outSayi);
+            }
+            else{
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(41,23, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot aşırı yükleme
+            int ifade = 1234;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Enes","Günümdoğdu");
+
         }
         static int Topla(int deger1,int deger2){
             return(deger1+deger2);
         }
-
-
     }
 
     class Metotlar{
@@ -43,6 +63,18 @@ namespace Methods
             deger1+=1;
             deger2+=1;
             return deger1+deger2;
+        }
+
+        public void Topla(int a, int b, out int toplam){
+    toplam = a+b;
+        }
+
+        public void EkranaYazdir(int veri){
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1, string veri2){
+            Console.WriteLine(veri1 + veri2);
         }
     }
 }   
